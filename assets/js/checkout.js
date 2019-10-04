46ffd69cc647b60ea70d5949062b59760952f503
cuireuncroco <46222664+cuireuncroco@users.noreply.github.com>
2019-10-04 16:38:08 +0200

    Add select to pricing page

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -3,7 +3,7 @@ var stripe = Stripe('pk_live_U2xwsrlYPffCBwDlCj988OUO00cjhcrOSN')
 var checkoutButtons = document.getElementsByClassName('stripe-checkout')
 
 for (var i = 0; i < checkoutButtons.length; i++) {
-  checkoutButtons[i].addEventListener('click', function() {
+  checkoutButtons[i].addEventListener('click', function () {
     var plan = 'plan_F5wdsqE4qJ4LPH' // default plan is monthly
     if (this && this.getAttribute('data-stripe-plan-id')) {
       plan = this.getAttribute('data-stripe-plan-id')
@@ -16,7 +16,7 @@ for (var i = 0; i < checkoutButtons.length; i++) {
         cancelUrl: 'https://mailmeteor.com/checkout/fail',
         allowIncompleteSubscriptions: true
       })
-      .then(function(result) {
+      .then(function (result) {
         if (result.error) {
           // If `redirectToCheckout` fails due to a browser or network
           // error, display the localized error message to your customer.
