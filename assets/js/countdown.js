1c71e3f01fa2ecc553b52dafe72376aa2067b856
Corentin Brossault <corentin.brossault@gmail.com>
2021-10-11 15:34:33 +0200

    fix: remove useless code

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,41 +1,37 @@
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
+  var countdowns = document.getElementsByClassName("promo-code-countdown");
 
-    let countdowns = document.getElementsByClassName("promo-code-countdown")
-console.log(countdowns)
+  // Countdown to next midnight
+  var countDownDate = new Date(new Date().setHours(24, 0, 0, 0));
 
-// Countdown to next midnight
-var countDownDate = new Date(new Date().setHours(24,0,0,0));
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
 
-  // Time calculations for days, hours, minutes and seconds
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  if (hours < 10) hours = "0" + hours;
-  if (minutes < 10) minutes = "0" + minutes;
-  if (seconds < 10) seconds = "0" + seconds;
-
-  // Display the result in the element with id="demo"
-  for (countdown of countdowns) {
-      countdown.innerHTML = hours + ":" + minutes + ":" + seconds;
-  }
-  
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
+    // Display the result in the element with id="demo"
     for (countdown of countdowns) {
-        countdown.innerHTML = "00:00:00";
+      countdown.innerHTML = hours + ":" + minutes + ":" + seconds;
     }
-  }
-}, 1000);
-})
 
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      for (countdown of countdowns) {
+        countdown.innerHTML = "00:00:00";
+      }
+    }
+  }, 1000);
+});
